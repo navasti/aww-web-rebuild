76a3ab3c3e915a0a153ba8fc2eedd8f3a8a4197c
Paweł Szostak <szostak_pawel@outlook.com>
2020-09-03 20:20:38 +0200

    Created canvas home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,24 @@
 import React from "react"
 import Layout from "../components/layout"
 
+// Components
+import HomeBanner from "../components/homePage/HomeBanner"
+
+// Contex
+import { useGlobalStateContext, useGlobalDispatchContext } from "../context/globalContext"
+
 const IndexPage = props => {
-  return <Layout></Layout>
+  const { cursorStyles } = useGlobalStateContext()
+  const dispatch = useGlobalDispatchContext()
+  const onCursor = cursorType => {
+    cursorType = (cursorStyles.includes(cursorType) && cursorType) || false
+    dispatch({ type: "CURSOR_TYPE", cursorType: cursorType })
+  }
+  return (
+    <Layout>
+      <HomeBanner onCursor={onCursor} />
+    </Layout>
+  )
 }
 
 export default IndexPage
